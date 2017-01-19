f5cbcdda2617916998d364da2472220f4730aaa9
Pinkel <pinkelgrg@hotmail.com>
2017-01-18 20:39:10 -0500

    update db

--- MYSQL/DockerData/setupDB.sql
+++ MYSQL/DockerData/setupDB.sql
@@ -1,4 +1,5 @@
 USE 5140Manilla;
+DROP TABLE IF EXISTS login;
 CREATE TABLE login (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   username varchar(50) NOT NULL,
@@ -7,10 +8,11 @@ CREATE TABLE login (
   modified_timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   UNIQUE KEY username (username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-use 5140Manilla;
-INSERT INTO login (username, password,created_timestamp) VALUES (pinkelgrg,)
+INSERT INTO login (username, password,created_timestamp) VALUES ('pinkelgrg','changeMe', NOW());
+
+
 use mysql;
-CREATE USER 'appuser' IDENTIFIED BY '5140Manilla';
+CREATE USER IF NOT EXISTS 'appuser' IDENTIFIED BY '5140Manilla';
 GRANT INSERT, UPDATE on 5140Manilla.login TO appuser;
